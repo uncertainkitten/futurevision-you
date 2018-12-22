01821b0a62bed9f58fc012b58bd20a910956daea
Cassandra <uncertainkitten@gmail.com>
2018-12-22 01:19:33 -0800

    Add authenticate_request to Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_request
+  attr_reader :current_user
+
+  private
+  def authenticate_request
+    @current_user = AuthorizeApiRequest.call(request.headers).result
+    render json: {error: "No can do bro, we can't be authorizing people willy nilly"}, status: 401 unless @current_user
+  end
 end
--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -7,7 +7,7 @@ class AuthenticationController < ApplicationController
     if command.success?
       render json: { auth_token: command.result }
     else
-      render json: { error: command.errors }, status: :unauthorized
+      render json: { error: command.errors }, status: 401
     end
   end
 end
